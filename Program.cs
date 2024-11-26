55a0e727f01a1f0c8f680790adef4881b10dbe7e
TheExiledCat <Kazekage2002@hotmail.com>
2024-11-26 11:06:40 +0100

    csv added

--- Program.cs
+++ Program.cs
@@ -1,32 +1,78 @@
 ﻿namespace io; // Definieert de namespace io
-
+using Dumpify;
 class Program // Definieert de Program klasse
 {
+    public static string fileName = "people.csv";
     static void Main(string[] args) // Hoofdfunctie van het programma
     {
-        List<Person> personen = []; // Initialiseer een lijst om personen op te slaan
-        for (int i = 0; i < 5; i++) // Loop om 5 personen in te voeren
-        {
-            Console.WriteLine("Wat is je naam?"); // Vraagt de gebruiker om hun naam
-            string naam = Console.ReadLine(); // Leest de naam in
-            Console.WriteLine("Wat is je leeftijd?"); // Vraagt de gebruiker om hun leeftijd
-            int leeftijd = int.Parse(Console.ReadLine()); // Leest en converteert de leeftijd naar een integer
-            Console.WriteLine("Wat is je lengte?"); // Vraagt de gebruiker om hun lengte
-            float lengte = float.Parse(Console.ReadLine()); // Leest en converteert de lengte naar een float
-            Person p = new Person(); // Maakt een nieuw Person object aan
-            p.Name = naam; // Stelt de naam in
-            p.Age = leeftijd; // Stelt de leeftijd in
-            p.Height = lengte; // Stelt de lengte in
-            personen.Add(p); // Voegt het Person object toe aan de lijst
-        }
-        SavePeople(personen); // Slaat de lijst van personen op in een bestand
-        List<Person> people = LoadPeople(); // Laadt de lijst van personen uit het bestand
+        Console.Clear();
+        Console.WriteLine("Wat is je naam?"); // Vraagt de gebruiker om hun naam
+        string naam = Console.ReadLine(); // Leest de naam in
+        Console.WriteLine("Wat is je leeftijd?"); // Vraagt de gebruiker om hun leeftijd
+        int leeftijd = int.Parse(Console.ReadLine()); // Leest en converteert de leeftijd naar een integer
+        Console.WriteLine("Wat is je lengte?"); // Vraagt de gebruiker om hun lengte
+        float lengte = float.Parse(Console.ReadLine()); // Leest en converteert de lengte naar een float
+        Person p = new Person(); // Maakt een nieuw Person object aan
+        p.Name = naam; // Stelt de naam in
+        p.Age = leeftijd; // Stelt de leeftijd in
+        p.Height = lengte; // Stelt de lengte in
+        AddPersonCSV(p);
+        List<Person> people = LoadPeopleCSV(); // Laadt de lijst van personen uit het bestand
         foreach (Person person in people) // Loop door elke persoon in de geladen lijst
         {
-            Console.WriteLine(person.Name + " " + person.Age + " " + person.Height); // Toont de gegevens van elke persoon
+            Console.BackgroundColor = ConsoleColor.DarkYellow;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write(person.Name.PadRight(20, '-') + "|");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.Write(person.Age.ToString().PadLeft(3, '-') + "|");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(person.Height.ToString("0.00").PadRight(3, '-'));
+            Console.ResetColor();
+
         }
     }
+    static void AddPersonCSV(Person person)
+    {
+        List<Person> existingPeople = LoadPeopleCSV();
+        existingPeople.Add(person);
 
+        StreamWriter writer = new StreamWriter(fileName);
+        writer.WriteLine("Name,Age,Height");
+        foreach (Person p in existingPeople)
+        {
+            writer.WriteLine($"{p.Name},{p.Age},{p.Height}");
+        }
+        writer.Close();
+    }
+    static List<Person> LoadPeopleCSV()
+    {
+        if (File.Exists(fileName))
+        {
+            StreamReader reader = new StreamReader(fileName);
+            reader.ReadLine();
+            List<Person> people = new List<Person>();
+            while (!reader.EndOfStream)
+            {
+                string regel = reader.ReadLine();
+                string[] data = regel.Split(",");
+                Person person = new Person
+                {
+                    Name = data[0],
+                    Age = int.Parse(data[1]),
+                    Height = float.Parse(data[2])
+                };
+                people.Add(person);
+            }
+            reader.Close();
+            return people;
+
+        }
+        else
+        {
+            return [];
+
+        }
+    }
     static void SavePeople(List<Person> people) // Methode om personen op te slaan
     {
         StreamWriter writer = new StreamWriter("people.txt"); // Opent een StreamWriter om naar people.txt te schrijven
